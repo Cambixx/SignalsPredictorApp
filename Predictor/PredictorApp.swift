b60e3efee3193a5e0a6531f805af1f9764daf2cf
Carlos Rabago <carlosmiguel40@gmail.com>
2024-12-05 22:24:12 +0100

    Initial Commit

---
+++ Predictor/PredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PredictorApp.swift
+//  Predictor
+//
+//  Created by Carlos Rábago on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PredictorTests/PredictorTests.swift
@@ -0,0 +1,17 @@
+//
+//  PredictorTests.swift
+//  PredictorTests
+//
+//  Created by Carlos Rábago on 5/12/24.
+//
+
+import Testing
+@testable import Predictor
+
+struct PredictorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
